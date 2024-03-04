b630714dc63daeb8aa1205820c53441a3c0903ad
hai <115616338+theoskoghntijohanneberg@users.noreply.github.com>
2024-03-04 15:02:34 +0100

    v10

--- app.rb
+++ app.rb
@@ -26,13 +26,19 @@ post('/buildarmy') do
     chaos = params[:chaos]
     necrons = params[:necrons]
 
-    if faction == imperium
-        db.execute('SELECT * faction WHERE faction_id = 1')
-    elsif faction == chaos
-        db.execute('SELECT * faction WHERE faction_id = 2')
-    elsif faction == necrons
-        db.execute('SELECT * faction WHERE faction_id = 3')
+    if faction == "imperium"
+        db.execute('SELECT * FROM faction WHERE faction_id = 1')
+        session[:faction] = 1
+        # db.execute('SELECT * FROM army INNER JOIN unit ON army.unit_id = unit.unit_id')
+    elsif faction == "chaos"
+        db.execute('SELECT * FROM faction WHERE faction_id = 2')
+        session[:faction] = 2
+    elsif faction == "necrons"
+        db.execute('SELECT * FROM faction WHERE faction_id = 3')
+        session[:faction] = 3
     end
+
+    
 
     redirect('/types')
 end
@@ -46,14 +52,24 @@ end
 get('/units/:id') do
     db = connect_db('db/hej.db')
     button = params[:id]
-    @unit_list = db.execute('SELECT * FROM unit WHERE type_id = ?',button)
+
+    if session[:faction] == 1
+        @unit_list = db.execute('SELECT * FROM unit WHERE faction_id = 1 AND type_id = ?',button)
+    elsif session[:faction] == 2
+        @unit_list = db.execute('SELECT * FROM unit WHERE faction_id = 2 AND type_id = ?',button)
+    elsif session[:faction] == 3
+        @unit_list = db.execute('SELECT * FROM unit WHERE faction_id = 3 AND type_id = ?',button)
+    end
+    p @unit_list
+    p session[:faction]
+    # @unit_list = db.execute('SELECT * FROM unit WHERE type_id = ?',button,)
     slim(:units)
 end
 
 post('/units/:id_unit') do
     db = connect_db('db/hej.db')
     button = params[:id_unit]
-    @army = db.execute('INSERT INTO army (unit_id) VALUES (?)',button) #Lägga till user_id
+    @army = db.execute('INSERT INTO army (unit_id, user_id, amount) VALUES (?, 0, 0)',button) #Lägga till user_id
     redirect('/units/:id')
 end
 
